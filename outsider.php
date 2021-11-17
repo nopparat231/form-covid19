b9708ed43594d71906be1ed778bcd77a953f47fb
Anue0159 <aungangster@gmail.com>
2021-11-17 14:50:22 +0700

    Test

--- outsider.php
+++ outsider.php
@@ -1,3 +1,68 @@
+<style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  font: 16px Arial;  
+}
+
+/*the container must be positioned relative:*/
+.autocomplete {
+  position: relative;
+  display: inline-block;
+}
+
+input {
+  border: 1px solid transparent;
+  background-color: #f1f1f1;
+  padding: 10px;
+  font-size: 16px;
+}
+
+input[type=text] {
+  background-color: #f1f1f1;
+  width: 100%;
+}
+
+input[type=submit] {
+  background-color: DodgerBlue;
+  color: #fff;
+  cursor: pointer;
+}
+
+.autocomplete-items {
+  position: absolute;
+  border: 1px solid #d4d4d4;
+  border-bottom: none;
+  border-top: none;
+  z-index: 99;
+  /*position the autocomplete items to be the same width as the container:*/
+  top: 100%;
+  left: 0;
+  right: 0;
+}
+
+.autocomplete-items div {
+  padding: 10px;
+  cursor: pointer;
+  background-color: #fff; 
+  border-bottom: 1px solid #d4d4d4; 
+}
+
+/*when hovering an item:*/
+.autocomplete-items div:hover {
+  background-color: #e9e9e9; 
+}
+
+/*when navigating through the items using the arrow keys:*/
+.autocomplete-active {
+  background-color: DodgerBlue !important; 
+  color: #ffffff; 
+}
+</style>
+
+
 <div class="container">
     <div class="section">
 
@@ -14,7 +79,7 @@
                 <div class="row">
 
 
-                    <form action="store.php" id="outsider_check">
+                    <form action="store.php" id="outsider_check"  >
 
                         <div class="input-field col s6">
                             <i class="material-icons prefix">date_range</i>
@@ -50,6 +115,7 @@
                             <input id="agency" name="agency" type="text" class="validate" required>
                             <label for="agency">ชื่อหน่วยงานที่มา</label>
                         </div>
+                        
                         <div class="input-field col s6">
                             <input id="location_out" name="location_out" type="text" class="validate" required>
                             <label for="location_out">สถานที่หน่วยงานสถานที่หน่วยงาน (อำเภอ & จังหวัด)</label>
@@ -266,6 +332,123 @@
         document.getElementById("date_report").innerHTML = "วันที่ " + datepicker_date + "  เวลา " + timepicker_date;
     }
 </script>
+<script>
+function autocomplete(inp, arr) {
+  /*the autocomplete function takes two arguments,
+  the text field element and an array of possible autocompleted values:*/
+  var currentFocus;
+  /*execute a function when someone writes in the text field:*/
+  inp.addEventListener("input", function(e) {
+      var a, b, i, val = this.value;
+      /*close any already open lists of autocompleted values*/
+      closeAllLists();
+      if (!val) { return false;}
+      currentFocus = -1;
+      /*create a DIV element that will contain the items (values):*/
+      a = document.createElement("DIV");
+      a.setAttribute("id", this.id + "autocomplete-list");
+      a.setAttribute("class", "autocomplete-items");
+      /*append the DIV element as a child of the autocomplete container:*/
+      this.parentNode.appendChild(a);
+      /*for each item in the array...*/
+      for (i = 0; i < arr.length; i++) {
+        /*check if the item starts with the same letters as the text field value:*/
+        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+          /*create a DIV element for each matching element:*/
+          b = document.createElement("DIV");
+          /*make the matching letters bold:*/
+          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+          b.innerHTML += arr[i].substr(val.length);
+          /*insert a input field that will hold the current array item's value:*/
+          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          /*execute a function when someone clicks on the item value (DIV element):*/
+          b.addEventListener("click", function(e) {
+              /*insert the value for the autocomplete text field:*/
+              inp.value = this.getElementsByTagName("input")[0].value;
+              /*close the list of autocompleted values,
+              (or any other open lists of autocompleted values:*/
+              closeAllLists();
+          });
+          a.appendChild(b);
+        }
+      }
+  });
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
+  function closeAllLists(elmnt) {
+    /*close all autocomplete lists in the document,
+    except the one passed as an argument:*/
+    var x = document.getElementsByClassName("autocomplete-items");
+    for (var i = 0; i < x.length; i++) {
+      if (elmnt != x[i] && elmnt != inp) {
+        x[i].parentNode.removeChild(x[i]);
+      }
+    }
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+      closeAllLists(e.target);
+  });
+}
+
+/*An array containing all the country names in the world:*/
+var countries = ["ส่วนบุคคล","บริษัทยาคูล","NNR","CKP","โซดิ","3M","TSU","Agilent","ปทุม","สเปเชียล","Fedex","ครินสแตท","DPX","มิซูมิ",
+"ธนากุล","รวมชัย","NK.","ISC","WMS","หจก ดวงตะวัน","ไห่หยาง","สยามโกเด้น","ดีเอชแอล","LSA PrintPack Alliance Co.,Ltd.",
+"บจก.ไทยแพ็คโซลูชั่น","บุญ​รอด​รีไซเคิล​","TRIPLE I LOGISTICS PCL","Saidai communication.","Rentokil",
+"TDE​","โอจิ","Hanna","ปินันชิน","TTL","UPS","เบตเตอร์","เทคเคม","จิตเจริญ","DHL","BIC","Sun108","วอเทอร์เน็ท","บ.เนริ์ดซิง",
+"โมซาร์ท พย","Covermat Co.,Ltd.","Nitto","โคเซอร์แมท","NT","เฟสเอ็ก","บมจ.สหมิตรเครื่องกล​","PST Clothing",
+"FreeDom Solution.","ยามาโตะ","SJ","SSA","ทีดีอี","PSV","AS shipping","ไทยน้ำทิพย์","ปานส่วนทิพย์",
+"SAS","AB9","เอเวอรี่","NTT Solution Thailand","Hilltech","PSC","SGS","SJN",
+"เป๊บซี่","Lyreco","Rotometrics","เอ็น เค","โพรเทคทีพ","สแนกทรูโก","UCI","NP Interpart",
+"ATT","TSG","เฟรนด์ชิพ","RT supply","WU.C.I","ส่งเรซิ่น","อัลฟ่าไดมอนด์","ชีว่า","สมัครงาน","BWT",
+"บ.วีอาร์เกรทแมชชินนารี่ จำกัด","กิจเจริญ","ไอยรา","NP","บีทาเก้น","บมจ.สหมิตรเครื่องกล","สปีดมิลด์","เสรีชัย","เมรอค",
+"บริษัท เวิลด์ ไฮเทค มาร์เก็ตติ้ง จำกัด","หจก.พิมพ์ศรีเทรดดิ้ง","ชัยปกรณ์​ภัณฑ์​","เฟรนชิพ","พศิน เอ็นจิเนียริ่ง","สวน",
+"บจก.เนอร์สซิ่ง เร็นเดอริ่ง","PwC Thailand","Micap","ร้านเต็นท์ชลบุรี","เป๊ปซี่","รพ อินเตอร์เมด","RT",
+"เร็นโทคิล","ยูซีไอ","สหมิตร","NSR","ยูชีไอ","อนันต์โชคแมทชีนเนอรี่","เทอร์มอลแพค","ซีรัสท์"];
+
+/*initiate the autocomplete function on the "myInput" element, and pass along the countries array as possible autocomplete values:*/
+autocomplete(document.getElementById("agency"), countries);
+</script>
+
 
 
 <br>
