25f7cb38272e2c4cb94219c4227bbc66a46fe0d4
Anue0159 <aungangster@gmail.com>
2022-02-16 22:28:18 +0700

    Update Home.php

--- Home.php
+++ Home.php
@@ -10,14 +10,14 @@
         <div class="row center">
             <a href="?insider" class="btn-large waves-effect waves-light orange">
                 <!-- <i class="material-icons left">assignment_ind</i> -->
-                สำหรับพนักงาน/Employee
+                พนักงาน/Employee
             </a>
         </div>
         <br>
         <div class="row center">
             <a href="?outsider" class="btn-large waves-effect waves-light teal">
                 <!-- <i class="material-icons left">contacts</i> -->
-                สำหรับบุคคลภายนอก/Visitor
+                บุคคลภายนอก/Visitor
             </a>
         </div>
         <br>
