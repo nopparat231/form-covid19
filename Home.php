7a095a288b48a6a1264f021148767b5c57d6f4e0
Anue0159 <aungangster@gmail.com>
2022-05-25 16:21:20 +0700

    Update Home.php

--- Home.php
+++ Home.php
@@ -11,21 +11,21 @@
         <div class="row center">
             <a href="?insider" class="btn-large waves-effect waves-light orange">
                 <!-- <i class="material-icons left">assignment_ind</i> -->
-                พนักงาน/Employee..
+                พนักงาน../Employee..
             </a>
         </div>
         <br>
         <div class="row center">
             <a href="?outsider" class="btn-large waves-effect waves-light teal">
                 <!-- <i class="material-icons left">contacts</i> -->
-                บุคคลภายนอก/Visitor..
+                บุคคลภายนอก../Visitor..
             </a>
         </div>
         <br>
         <div class="row center">
             <a href="/Privacy.php" class="btn-large waves-effect waves-light teal">
                 <!-- <i class="material-icons left">contacts</i> -->
-                Privacy Notice
+                Privacy Notice..
             </a>
         </div>
 
